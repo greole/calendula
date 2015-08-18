bdb028c0b5ea28ef28e19b5b0221c6c3f1596cde
gregor <gregor.olenik@web.de>
2015-08-18 22:03:48 +0200

    add cabal file and example folder

--- Calendula.hs
+++ Calendula.hs
@@ -1,53 +1,74 @@
 import Data.List (isInfixOf)
 import Data.String.Utils (replace)
-import System.FilePath.Find
-import System.FilePath.Posix (makeRelative, takeDirectory)
+import System.FilePath.Find (
+    fileType, find, (==?), directory, (/=?), FileType(..), FindClause)
+import System.FilePath.Posix (makeRelative, takeDirectory, takeFileName)
 import System.Directory (getCurrentDirectory, copyFile)
 import System.Environment (getExecutablePath)
-import Text.Regex.Posix
+import Text.Regex.Posix ((=~))
 import Text.Pandoc
-import Control.Monad
-import Control.Monad (zipWithM_)
+import Control.Monad (zipWithM_, forM_, forM)
+import Paths_calendula
 
 htmlHeader = "<!DOCTYPE html><html><body><head><link rel=\"stylesheet\" \
      \ type=\"text/css\" href=\"style.css\"></head><div class=\"outer\"> \
      \ <div class=\"toc\"></div><div class=\"wrapper\">"
 
-htmlFooter = "</div></div><script src=\"jquery-2.1.4.min.js\"></script><script\
-   \ src=\"toc.min.js\"></script><script>$('.toc').toc({});</script></body></html>"
+htmlFooter = "</div></div><script src=\"jquery.min.js\"></script><script\
+   \ src=\"toc.min.js\"></script><script>$('.toc').toc({'selectors': 'h1,h2,h3,h4'});</script></body></html>"
 
 notGit :: FindClause Bool
 notGit = directory /=? ".git"
 
 -- First scan complete file system for markdown files
 buildIndex :: FilePath -> IO [FilePath]
-buildIndex = find notGit (fileType ==? RegularFile &&? extension ==? ".md")
+buildIndex = find notGit (fileType ==? RegularFile)
 
 concatMarkdown :: FilePath -> [FilePath] -> IO ()
 concatMarkdown outFile files = do 
         writeFile outFile htmlHeader
-        forM_ files $ \infile -> do
+        forM_ (filter isMDfile files) $ \infile -> do
             contents <- readFile infile
-            appendFile outFile $ markdownToHtml $ "#" ++  fileToHeader infile 
-            appendFile outFile $ markdownToHtml contents 
+            level <- subLevel infile
+            appendFile outFile $ wrapDiv . markdownToHtml $ 
+                incHeaderLevel level contents
         appendFile outFile htmlFooter
 
-fileToHeader :: String -> String
-fileToHeader x = if (x =~ "index.md" :: Bool)
-        then (x =~ "[A-Za-z ]*/index.md" :: String) =~ "[A-Za-z ]*" :: String
-        else (x =~ "[A-Za-z ]*.md" :: String) =~ "[A-Za-z ]*" :: String
+subLevel :: FilePath -> IO Int
+subLevel x = do
+       cwd <- getCurrentDirectory
+       return $ (count '/' (makeRelative cwd x)) + (index x) 
+            where count c = length . filter (==c)
+                  index :: FilePath -> Int
+                  index c = if c =~ "index.md" :: Bool
+                            then 0
+                            else 1
+
+incHeaderLevel :: Int -> String -> String
+incHeaderLevel i = replace "#" $ replicate (i+1) '#'
+
+isMDfile :: String -> Bool
+isMDfile = (=~ ".md")
+
+isContent :: String -> Bool
+isContent = not . (=~ ".md|.html")
 
 markdownToHtml :: String -> String
 markdownToHtml = (writeHtmlString def {writerReferenceLinks = True}) . readMarkdown def
 
+wrapDiv :: String -> String
+wrapDiv x = "<div class=\"article\">" ++ x ++ "</div>"
+
 main = do
     let targetDir = "/tmp/Calendula/"
-    exec <- getExecutablePath
     cwd <- getCurrentDirectory
     index <- buildIndex cwd
-    let assets = ["style.css", "jquery-2.1.4.min.js", "toc.min.js"]
-    let assetsSrc = map ((takeDirectory exec ++ "/assets/") ++ ) assets
+    let assets = ["style.css", "jquery.min.js", "toc.min.js"]
+    assetsSrc <- forM (map ("assets/" ++) assets) getDataFileName 
+    putStrLn (show assetsSrc)
     let assetsDst = map (targetDir ++ ) assets
-    putStrLn $ show assets
+    let mediaSrc = filter isContent index 
+    let mediaDst = map ((targetDir ++) . takeFileName) mediaSrc
     zipWithM_ copyFile assetsSrc assetsDst
+    zipWithM_ copyFile mediaSrc mediaDst
     concatMarkdown (targetDir ++ "index.html") index
---
+++ example/000index.md
@@ -0,0 +1,14 @@
+# Calendula
+
+**Calendula** is a *puristic* static website generator written in [Haskell](https://www.haskell.org/)
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+A picture
+<img src="cc.png" alt="CC-Logo" width="250"></img>
+
+Some Code
+
+~~~
+cat /dev/random
+~~~
